676b0037a824c3398ebdf0fc3a4ff915bcf5e593
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-10-08 23:51:42 +0300

    Persist data when page is reloaded

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-document.addEventListener( "DOMContentLoaded", () => {
-    const addButton = document.getElementById( 'add-task-btn' );
-    const taskInput = document.getElementById( 'task-input' );
-    const taskList = document.getElementById( 'task-list' );
+const addButton = document.getElementById( 'add-task-btn' );
+const taskInput = document.getElementById( 'task-input' );
+const taskList = document.getElementById( 'task-list' );
 
-    function addTask () {
-        let taskText = taskInput.value.trim();
-        if ( taskText === "" ) {
-            alert("Enter a task!")
-        } else {
-            let li = document.createElement( 'li' );
-            li.textContent = taskText;
-            let removeButton = document.createElement( 'button' );
-            removeButton.textContent = 'Remove';
-            removeButton.classList.add( 'remove-btn' );
-            removeButton.addEventListener( 'click', () => {
-                li.remove();
-            } )
-            li.appendChild( removeButton );
-            taskList.appendChild( li );
-            taskInput.value = "";
-        }
+
+function addTask ( defaultTask, save = true ) {
+    let taskText = defaultTask || taskInput.value.trim();
+    if ( taskText === "" ) {
+        return alert("Enter a task!")
+    } else {
+        let li = document.createElement( 'li' );
+        li.textContent = taskText;
+
+        let removeButton = document.createElement( 'button' );
+        removeButton.textContent = 'Remove';
+        removeButton.classList.add( 'remove-btn' );
+        removeButton.addEventListener( 'click', () => {
+            li.remove();
+        } )
+        li.appendChild( removeButton );
+        taskList.appendChild( li );
+        taskInput.value = "";
     }
+    if ( save ) {
+        allTasks = JSON.parse( localStorage.getItem( 'allTasks' ) ) || [];
+        allTasks.push( taskText );
+        localStorage.setItem( 'allTasks', JSON.stringify( allTasks ) );
+    }
+}
 
-    addButton.addEventListener( 'click', addTask );
-    taskInput.addEventListener( 'keypress', ( event ) => {
-        if ( event.key === "Enter" ) {
-            addTask();
-        }
-    } )
-    
-    addTask()
-} );
+addButton.addEventListener( 'click', ()=> addTask() );
+taskInput.addEventListener( 'keypress', ( event ) => {
+    if ( event.key === "Enter" ) {
+        addTask();
+    }
+} )
+
+function loadTasks () {
+    const listOfTasks = JSON.parse( localStorage.getItem( 'allTasks' ) ) || [];
+    console.log(listOfTasks)
+    listOfTasks.forEach(task => {
+        addTask( task, false);
+    });
+}
+
+document.addEventListener( "DOMContentLoaded", loadTasks);
+
+
 
